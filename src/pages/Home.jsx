7ba1e5262ef1d5ecd94264b960d5e7406f4fff69
reihan yarjanlou <r.yarjanlouuu80@gmail.com>
2025-04-09 16:09:43 +0330

    my changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { RiAdminFill } from "react-icons/ri";
+import { RiAdminFill, RiLoginBoxFill, RiLoginBoxLine } from "react-icons/ri";
 import { SiFreelancer } from "react-icons/si";
 import { FaUserCheck } from "react-icons/fa";
 import { Link } from "react-router-dom";
@@ -9,14 +9,17 @@ function Home() {
       <div className="container space-y-8 pt-10">
         <h1 className="text-xl font-bold">صفحه اصلی</h1>
         <div className="space-y-4">
+          <HomeLink to="/auth" text="ثبت نام یا ورود به حساب کاربری">
+            <RiLoginBoxFill className="size-[18px]" />
+          </HomeLink>
           <HomeLink to="/admin" text="پنل ادمین">
-            <RiAdminFill />
+            <RiAdminFill className="size-[18px]" />
           </HomeLink>
           <HomeLink to="/owner" text="پنل کارفرما">
-            <FaUserCheck />
+            <FaUserCheck className="size-[18px]" />
           </HomeLink>
           <HomeLink to="/freelancer" text="پنل فریلنسر">
-            <SiFreelancer />
+            <SiFreelancer className="size-[18px]" />
           </HomeLink>
         </div>
       </div>
